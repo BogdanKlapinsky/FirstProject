931e1f4467e68fa1a89f7914aadf57af9e0bc642
Богдан <Bogd555k@gmail.com>
2016-08-02 19:46:45 +0300

    GitFirst

--- test/Main.java
+++ test/Main.java
@@ -1,9 +1,7 @@
 package ua;
-
 public class Main {
-
-	public static void main(String[] args) {
-		System.out.println("Hello");
+	public static void main(String[] args){
+		System.out.println("People");
+		System.out.println("Hiii");
 	}
-
 }
